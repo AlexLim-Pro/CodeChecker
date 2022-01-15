dfee3981ab4fff152b5e6bfa3b9c73fc9c506162
Alex Lim <74800585+AlexLim-Pro@users.noreply.github.com>
2022-01-14 19:00:03 -0800

    updating code

--- CheckBinarySearchTreeSequence.py
+++ CheckBinarySearchTreeSequence.py
@@ -5,21 +5,22 @@ def CheckBinarySearchTreeSequence(arr: iter):
   :param arr: An array containing the binary search tree sequence
   :type arr: iter
   """
+  invalidSequenceString = "sequence is invalid: %s %s %s."
   searchVal = arr[-1]
   minVal = min(arr) - 1
   maxVal = max(arr) + 1
   for i in arr:
     if i < searchVal:
       if i < minVal:
-        return i
+        return invalidSequenceString % (i, "<", minVal)
       if i == minVal:
-        return i
+        return invalidSequenceString % (i, "appears", "multiple times")
       minVal = i
     elif i > searchVal:
       if i > maxVal:
-        return i
+        return invalidSequenceString % (i, ">", maxVal)
       if i == maxVal:
-        return i
+        return invalidSequenceString % (i, "appears", "multiple times")
       maxVal = i
-  print("no issues")
+  return "the sequence is valid."
 
